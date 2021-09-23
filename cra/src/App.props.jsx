1f8bc2de85a37ab137065953fb6acd7d178c3665
kanglaeheon <kanglaeheon@gmail.com>
2021-09-23 14:16:34 +0900

    Props in Function Component

--- cra/src/App.props.jsx
+++ cra/src/App.props.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropClass from './components/PropClass';
+import PropFunc from './components/PropFunc';
 
 function App() {
     const messageFromChild = (message) => {
@@ -17,7 +18,20 @@ function App() {
                     name: "홍길동",
                     age: 28
                 }} 
-                prop_func={ messageFromChild }/>
+                prop_func={ messageFromChild }
+                />
+            <h2>Props in Function Component</h2>
+            <PropFunc
+                prop_str="string value"
+                prop_num={ 10 }
+                prop_false={ false }
+                prop_true
+                prop_obj={{
+                    name: "홍길동",
+                    age: 28
+                }}
+                prop_func={ messageFromChild }
+                />
         </div>
     )
 }
