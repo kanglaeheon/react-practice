f060e55000d0ab0a82a0fbf45be201fc8789d723
kanglaeheon <kanglaeheon@gmail.com>
2021-09-24 13:45:48 +0900

    Context API

---
+++ cra/src/App.Context.jsx
@@ -0,0 +1,12 @@
+import ContextApi from './components/ContextApi';
+
+function App() {
+    return (
+        <div>
+            <h2>Using Context API</h2>
+            <ContextApi />
+        </div>
+    )
+}
+
+export default App;
