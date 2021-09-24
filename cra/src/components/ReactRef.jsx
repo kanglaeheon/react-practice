7a5f2fa16a3ec98e909c972c31f4089b2df48bc4
kanglaeheon <kanglaeheon@gmail.com>
2021-09-24 13:14:08 +0900

    Ref 사용하기

--- cra/src/components/ReactRef.jsx
+++ cra/src/components/ReactRef.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class ReactRef extends Component {
     constructor(props) {
         super(props);
-
+        //  참조 객체 생성: DOM 요소에 엘리먼트를 저장하기 위한 Ref
+        this.textInput = React.createRef();
+        //  input#id 엘리먼트 참조
     }
 
     JavaScriptFocus() {
@@ -12,11 +14,22 @@ class ReactRef extends Component {
         document.getElementById("id").focus();
     }
 
+    RefFocus = (e) => {
+        //  Ref를 이용한 엘리먼트 접근
+        console.log("REF: ", this.textInput);
+        console.log(this.textInput.current);
+        //  DOM 노드를 얻기 위해, current 속성 이용
+        this.textInput.current.style.backgroundColor = "lightgreen";
+        this.textInput.current.focus();
+    }
+
     render() {
+        console.log("REF: ", this.textInput);
         return (
             <>
-                <input id="id" type="text" />
-                <input type="button" value="JS Focus" onClick={ this.JavaScriptFocus } />
+                <input id="id" type="text" ref={ this.textInput } />
+                <input type="button" value="VJS Focus" onClick={ this.JavaScriptFocus } />
+                <input type="button" value="Ref Focus" onClick={ this.RefFocus } />
             </>
         )
     }
