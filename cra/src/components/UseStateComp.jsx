b21550db1e565a53779a858f9202a680e6b44c1b
kanglaeheon <kanglaeheon@gmail.com>
2021-09-24 10:06:58 +0900

    setCount

--- cra/src/components/UseStateComp.jsx
+++ cra/src/components/UseStateComp.jsx
@@ -8,6 +8,8 @@ function UseStateComp(props) {
         <div>
             <h3>{ contents }</h3>
             <p>현재 카운트: { count } 입니다.</p>
+            <button onClick={() => setCount(count + 1 )}>+1</button>
+            <button onClick={() => setCount(count - 1 )}>-1</button>
         </div>
     )
 }
